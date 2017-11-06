5ca7ef2fd2ae9b8a01df5d8a97a4283f774b6dd3
Andrew Ko <andrewko@Andrews-MacBook-Pro.local>
2017-11-06 17:00:24 -0600

    rough draft/testing

---
+++ assets/javascript/app.js
@@ -0,0 +1,226 @@
+var config = {
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAHg6k5_WoMsK4BHqQoI_MfsBZsvsVUe0A",
+    authDomain: "hoppyhour-c711e.firebaseapp.com",
+    databaseURL: "https://hoppyhour-c711e.firebaseio.com",
+    projectId: "hoppyhour-c711e",
+    storageBucket: "hoppyhour-c711e.appspot.com",
+    messagingSenderId: "561885607376"
+  };
+  firebase.initializeApp(config);
+
+var database = firebase.database();
+
+var city = "";
+var state = "";
+var centerLat = 32.7767;
+var centerLong = -96.7970;
+
+var center_of_dallas = {
+    lat:  32.7767,
+    lng: -96.7970
+}
+
+//the ID for each marker in firebase
+var currentMarkerId;
+var rating;
+var markers;
+var idCounter = 0;
+var brewery_locations = [{}];
+
+var starRating = document.getElementById("starRating").cloneNode(true);
+
+$("#locationBtn").on("click",function(event){
+    event.preventDefault();
+
+    city = $("#city").val().trim();
+    state = $("#state").val().trim();
+
+    var googleURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + city + "," + state + "&key=AIzaSyAL1B97DDVFjIxxJ1MiQdM6GY68WmrSaRo"
+
+    $.ajax({
+        url: googleURL,
+        method: "GET"
+    }).done(function(responseGoogle){
+        var googleResults = responseGoogle;
+
+        centerLat = parseFloat(googleResults.results[0].geometry.location.lat);
+        centerLong = parseFloat(googleResults.results[0].geometry.location.lng);
+
+        var locations = getBreweryLocations(centerLat, centerLong);
+        initMap();
+        
+    })
+})
+
+
+function getBreweryLocationsCallPromise(lat, lng) {
+    var queryURL = "http://127.0.0.1:3000/breweries" + "?lat=" + String(lat) + "&lng=" + String(lng);
+
+           return $.ajax({
+               url: queryURL,
+                method: "GET",
+           });
+        }
+        // https://stackoverflow.com/questions/5316697/jquery-return-data-after-ajax-call-success
+        
+function processBreweryLocationsData(promise) {
+    promise.success(function(data) {
+        var data_object = JSON.parse(data);
+
+        brewery_locations = [{}];
+
+        var results = data_object.data;
+        for (var i=0; i < results.length; i++) {
+
+            brewery_locations[i] = {
+                location: {
+                    lat: results[i].latitude,
+                    lng: results[i].longitude
+                },
+
+                name: results[i].brewery.name,
+                url: results[i].brewery.website
+            };
+        };
+        initMap();
+    });
+}    
+
+function getBreweryLocations(lat, lng) {
+    var promise = getBreweryLocationsCallPromise(lat, lng);
+    processBreweryLocationsData(promise);
+}
+
+
+
+function initMap(locations) {
+    function addMarker(dataset, map_object) {
+        var marker = new google.maps.Marker({
+            animation: google.maps.Animation.DROP,
+            position: dataset.location,
+            map: map_object,
+            url: dataset.url,
+            name: dataset.name,
+            phone: dataset.phone,
+            image: dataset.icon
+        });
+        return marker
+    }
+
+    var center_of_map = {
+        lat: centerLat,
+        lng: centerLong
+    };
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: center_of_map
+    });
+
+    markers = brewery_locations.map(function(location) {
+        return addMarker(location, map)
+    });
+
+    markers.forEach(function(marker) {
+        google.maps.event.addListener(
+            marker,
+            'click',
+
+            //Once you click on a marker, it gets the "e" event which contains the lat and long which is then used as the ID 
+            function(e) {
+                //Make the lat and long into a string and replace the "." with "?"
+                //lat.Lng.lat() comes from google maps API.
+
+                //BUG with currentMarkerId
+                currentMarkerId = JSON.stringify(e.latLng.lat() + e.latLng.lng()).replace(".", "?");
+                
+                $("#breweryReview").empty();
+                $("#breweryName").html(marker.name + "<br>" + "<a href=\"" + marker.url + "\"" + "target=\"_blank\"" + ">" + marker.url + "</a>" + "<br>" + marker.image);
+                $("#initialInfo").css({
+                    "display": "none"
+                });
+                $("#breweryInfo").css({
+                    "display": "initial"
+                });
+
+                 var stars = $("#starRating").rateYo({
+                    starWidth: "18px",
+                 })
+
+                // turns off previous click event to not have repetitive values 
+                $("#reviewBtn").off('click');
+                $("#reviewBtn").on("click", function(event) {
+                    event.preventDefault();
+
+                    var reviewText = $("#reviewText").val().trim();
+                    var rating = $("#starRating").rateYo("rating");
+                    $("#starRating").rateYo("rating", 0);
+
+                    
+                    var reviewDB = {
+                        review: reviewText,
+                        rating: rating
+                    };
+                    //the .child lets you add branches 
+                    database.ref()
+                        .child("markers")
+                        .child(currentMarkerId)
+                        .push(reviewDB)
+                        //once data is pushed to firebase, it appends the newest review to #breweryReview
+                        .then(function() {
+                           var parentDiv = $("<div id=" +idCounter + ">");
+                           var stars = $("<div class=starReview>").rateYo({
+                                starWidth: "18px",
+                                rating: reviewDB.rating,
+                                readOnly: true
+                           })
+                           var review = $("<p>").text(reviewDB.review);
+                           $(parentDiv).append(stars);
+                           $(parentDiv).append(review);
+                           $("#breweryReview").append(parentDiv);
+                           idCounter += 1;
+                        })
+
+                    $("#reviewText").val("");
+                });
+
+                database.ref()
+                    .child("markers")
+                    .child(currentMarkerId)
+                    //It gets all the previous values in firebase only one time
+                    .once("value", function(childSnapshot, prevChildKey) {
+
+                        idCounter = 0;
+                        var data = childSnapshot.val();
+                        //if data is undefined, then dont do anything
+                        if(!data) {
+                            return;
+                        }
+                        //This gives the values of the object
+                        var reviews = Object.values(data)
+                        //forEach goes through the reviews array and runs the function in each of the items in reviews
+                        reviews.forEach(function(reviewObj) {
+                            var parentDiv = $("<div id=" +idCounter + ">");
+                           var stars = $("<div class=starReview>").rateYo({
+                                starWidth: "18px",
+                                rating: reviewObj.rating,
+                                readOnly: true
+                           })
+                           var review = $("<p>").text(reviewObj.review);
+                           $(parentDiv).append(stars);
+                           $(parentDiv).append(review);
+                           $("#breweryReview").append(parentDiv);
+                           idCounter += 1;
+                        });
+                        
+                    });
+                    
+            }
+        );
+    });
+
+}
+
+var markers = getBreweryLocations(center_of_dallas.lat, center_of_dallas.lng)
---
+++ server.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var cors = require('cors');
+var app = express();
+var request = require('request');
+var path = require('path');
+var server = require('http').createServer(app);
+var PORT = process.env.PORT || 3000;
+
+// define the path to use style sheets & images - public folder
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.use(cors());
+app.options('*', cors());
+
+var center_of_dallas = {
+    lat: 32.7767,
+    lng: -96.7970
+}
+
+function generateBrewAPIUrl(lat, lng) {
+    return "https://api.brewerydb.com/v2/search/geo/point/?lat=" + lat + "&lng=" + lng + "&key=1355791a4243a1ff090ed2643e281714&format=json";
+}
+
+
+app.get('/', function(req, res, next) {
+    // https://codeforgeek.com/2015/01/render-html-file-expressjs/
+    // res.sendFile(path.join(__dirname + '/public/brew.html'))
+    console.log('an example of how to serve a file');
+    res.sendFile(path.join(__dirname, "index.html"));
+})
+
+app.get('/breweries', function(req, res, next) {
+    // https://stackoverflow.com/questions/8515872/simple-api-calls-with-node-js-and-express
+
+    let lat = req.query.lat;
+    let lng = req.query.lng;
+    if (!lat || !lng) {
+        // console.log('didnt get nuttin');
+        // console.log(lat);
+        // console.log(lng);
+        var url = generateBrewAPIUrl(center_of_dallas.lat, center_of_dallas.lng);
+    } else {
+        var url = generateBrewAPIUrl(lat, lng);
+    }
+    console.log('url');
+    console.log(url);
+    // let url = generateBrewAPIUrl(centerLat, centerLong);
+    request(url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          // https://stackoverflow.com/questions/19696240/proper-way-to-return-json-using-node-or-express
+          res.json(body);
+      } else {
+          res.send(error);
+      }
+    });
+
+})
+
+// required if you wanna serve a file
+// app.use(express.static(__dirname + '/public'));
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
+// server.listen(3000);
+// console.log("--------------------------------");
+// console.log("Connected to Brewery App Server");
+// console.log('');
+// console.log("Keep this server running to allow functionability with the app.\n If the server is not running the API will not work.\n No connection = no dropped pins.");
